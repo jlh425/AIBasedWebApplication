b5f0e4894d49c6d9201ed32cd23b5baa502c31c8
jlh425 <105890316+jlh425@users.noreply.github.com>
2023-05-30 17:20:06 +0000

    centering app

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
         <div className='gradient' />
       </div>
 
-      <div className='app'>
+      <div className='app items-center'>
         <Hero />
         <Demo />
       </div>
