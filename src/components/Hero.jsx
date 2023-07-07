cf70ea29a983aa810971ab50dd3795e03cec4b28
jlh425 <105890316+jlh425@users.noreply.github.com>
2023-07-07 07:25:18 -0600

    centering hero comp

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import {logo} from '../assets';
 
 const hero = () => {
   return (
-    <header className='w-full flex-center justify-center items-center flex-col'> 
+    <header className='mx-8 w-full flex-center justify-center items-center flex-col'> 
         <nav className='flex justify-between items-center w-full mb-10 pt-3'>
             <img src={logo} alt="sumz_logo" className='w-28 object-contain'/>
             <button type='button' onClick={() => window.open('https://github.com/jlh425/AIBasedWebApplication')} className='black_btn'>
